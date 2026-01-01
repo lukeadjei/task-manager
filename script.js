41a8ac50136620440157b5cdb040586d80da68f8
Luke Adjei <lukeadjei101@gmail.com>
2026-01-01 18:36:58 -0500

    comit

---
+++ script.js
@@ -0,0 +1,41 @@
+const input = document.getElementById("taskInput");
+const button = document.getElementById("addBtn");
+const list = document.getElementById("taskList");
+
+
+function addItemToList (){
+    const taskText = input.value;
+
+    if (taskText.trim() === ""){
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "DELETE";
+
+    deleteBtn.addEventListener("click", () =>{
+        list.removeChild(li);
+    });
+
+    li.addEventListener("click", () => {
+        list.removeChild(li)
+    });
+
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+
+    input.value = "";
+
+}
+
+button.addEventListener("click", addItemToList);
+input.addEventListener("keydown", (event) =>{
+    if(event.key === "Enter"){
+        addItemToList();
+    }
+});
+
+
