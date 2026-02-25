379cfb7f095a6c1e82bbf74b5001a1dcc51c309b
luke <lukeadjei101@gmail.com>
2026-02-25 00:37:37 -0500

    added mongoDB database, setup env to hold keys, setup mongoose as bridge to connect to mongoDB, added try catch and async to different funcs

--- server.js
+++ server.js
@@ -1,31 +1,50 @@
-//Server Setup creation of express package
+//packages
+require('dotenv').config(); //this is getting the .env file and giving everything access to keys in the node js code
+const mongoose = require("mongoose");
 const express = require("express");
+//Server Setup creation of express package
 const app = express();
 const PORT = 3000;
+//Middleware meaning the functions that get applied to request before heading to main things
+app.use(express.json());
+
+//database setup creation of mongoose package
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected!")).catch((error) => console.log("There was an error trying to connect to the database", error));
 
 //array to hold task
 const taskDataBaseArray = [];
 
+//Creates a blueprint for data
+const taskSchema = new mongoose.Schema({
+    text: String,
+    completed: Boolean
+});
 
-//Middleware meaning the functions that get applied to request before heading to main things
-app.use(express.json());
-
-
+//This will create a model or obnject based on the blueprint or the schema
+const Task = mongoose.model('Task', taskSchema);
 
 //starting the server
 app.listen(PORT, () => {
-    console.log('Server is running on local host with the port:${PORT}');
+    console.log(`Server is running on local host with the port:${PORT}`);
 })
 
 // route to retrieve information specifically tasks
-app.get("/tasks", (req, res) => {
-    res.json(taskDataBaseArray);
-})
+app.get("/tasks", async (req, res) => {
+    try{
+        const tasks = await Task.find();
+        res.json(tasks);
+    }catch (error){
+        res.status(500).json({message: "Error trying to find task"});
+    }
+});
 
 //route to update or sending data to server
-app.post("/tasks", (req, res) => {
-    const task = req.body; //this is the js data that was sent
-    taskDataBaseArray.push(task); //putting inside of "fake database"
-    res.status(201).json(task);
-})
+app.post("/tasks", async (req, res) => {
+    try{
+        const sentData = await Task.create(req.body);
+        res.status(201).json(sentData);
+    }catch(error){
+        res.status(500).json({message: "Server Error"});
+    }
+});
 
