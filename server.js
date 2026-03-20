6d0ac2f4f61081ba37f600beacf2a22431bcbed1
luke <lukeadjei101@gmail.com>
2026-03-19 23:35:55 -0400

    created registration routed using bcrypt to hash password and create a new user with with user schema into the database

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require("express");
 const cors = require("cors");
 const Task = require("./models/Tasks");
 const path = require("path");
+const User = require("./models/User");
+const bcrypt = require ("bcrypt");
 //Server Setup creation of express package
 const app = express();
 const PORT = 3000;
@@ -87,3 +89,32 @@ app.delete("/tasks/:id", async (req, res) => {
     }
 });
 
+
+
+
+
+//Registration Routes
+app.post("/register", async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create new user
+        const newUser = await User.create({
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({message: "User Created Successfully", userId: newUser._id});
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" });
+    }
+});
